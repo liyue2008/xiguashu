1cea3af31ebb9ae8e123186a869ef3d6bfba63b5
LiYue <liyue2008@gmail.com>
2022-10-15 17:57:07 +0800

    增加了对连续值的支持

--- chapter_4/decision_tree.py
+++ chapter_4/decision_tree.py
@@ -1,7 +1,9 @@
 #-*-coding:utf-8-*- 
+from cProfile import label
 import math
 from platform import node
 from collections import Counter
+from typing import Tuple
 import pandas as pd
 
 def all_same(items):
@@ -12,6 +14,27 @@ def majority_in_list(l: list):
     """
     c = Counter(l)
     return c.most_common()[0][0]
+class Attribute:
+    """AttributeSet表示一个样本的一个属性，包含属性名称和属性的全部类别(取值范围)
+
+    Attributes
+    ----------
+    name : str
+        属性名称;
+    values : set
+        属性的全部类别(取值范围).
+    is_continuous : bool
+        属性是离散值(False)还是连续值(True), 默认为False离散值.
+    """
+    def __init__(self, name: str = '', values: set = set(), is_continuous: bool = False) -> None:
+        self.name = name
+        self.values = values
+        self.is_continuous = is_continuous
+    def __str__(self) -> str:
+        return self.name
+    def __repr__(self) -> str:
+        return self.name
+        
 
 class TrainingSet:
     """TrainingSet表示一个训练集，包含训练数据集和数据的标记。
@@ -34,10 +57,28 @@ class TrainingSet:
     def len(self) -> int:
         return len(self.samples.index)
 
-    def subset_by_attr(self, attr_name: str, attr_value):
-        """计算训练集 D 中在属性 attr_name 上取值为 attr_value 的样本子集
+    def partition_by_attr(self, a: Attribute):
+        """计算训练集 D 中在属性 attr_name 上取值为 attr_values 的样本子集
+        Returns
+        -------
+        返回一个Dictionary, key是属性取值, value是对应的D的子集.
+
         """
-        return TrainingSet(self.samples[self.samples[attr_name] == attr_value], self.label_name)
+        ret = {}
+        for attr_value in a.values:
+            ret[attr_value] = TrainingSet(self.samples[self.samples[a.name] == attr_value], self.label_name)
+        return ret
+    def bi_partition_by_attr(self, attr_name: str, t: float):
+        """二分法计算训练集 D 中在属性 attr_name 上,基于最佳划分点t划分的二个子集.
+
+        Returns
+        -------
+        返回包含二个元素的Dictionary: {'t-': D在属性a上取值不大于t的子集 D-, 't+': D在属性a上取值大于t的子集 D+).
+        """
+        return {
+            ('%.3f-' % t): TrainingSet(self.samples[self.samples[attr_name] <= t], self.label_name),
+            ('%.3f+' % t): TrainingSet(self.samples[self.samples[attr_name] > t], self.label_name)
+            }
     def all_samples_same(self, A: set) -> bool:
         """训练集D上是否所有样本在属性列表A中所有属性上取值相同
 
@@ -56,24 +97,6 @@ class TrainingSet:
                 return False
         return True
 
-class Attribute:
-    """AttributeSet表示一个样本的一个属性，包含属性名称和属性的全部类别(取值范围)
-
-    Attributes
-    ----------
-    name : str
-        属性名称;
-    values : set
-        属性的全部类别(取值范围).
-    """
-    def __init__(self, name: str = '', values: set = set()) -> None:
-        self.name = name
-        self.values = values
-    def __str__(self) -> str:
-        return self.name
-    def __repr__(self) -> str:
-        return self.name
-        
 class DecisionTreeNode:
     """DecisionTreeNode表示一个一个决策树的节点和它的所有子节点，实际上也是一个决策树。
 
@@ -159,8 +182,33 @@ def ent(D: TrainingSet) -> float:
     
     return - negative_ent
 
-def gain(D: TrainingSet, a: Attribute) -> float:
-    """计算样本 D 在属性 a 上的信息增益(information entropy).
+def gain_continuous(D: TrainingSet, a: Attribute) -> Tuple[float, float]:
+    """计算样本 D 在连续值属性 a 上, 采用二分法(bi-partition)的最优划分点t, 并返回基于t的二分信息增益.
+    """
+    # 将属性 a 在训练集 D 上的所有样本取值转换成list并排序
+    sorted_values = D.samples[a].values.tolist().sort()
+
+    # 计算 sorted_values 上每相邻二个点的中位值(候选划分点)集合 Ta
+
+    gain = -1
+    t = -1 # 最优划分点
+    for i in range(len(sorted_values) - 1):
+        ti = (sorted_values[i] + sorted_values[i + 1])/2
+        # 复制一份 D, 将 属性 a 的取值按照 t 做二分, 变成{不大于t(Dt-), 大于t(Dt+)}的二分离散值
+        Dta = TrainingSet(D.samples.copy(), label)
+        for i, row in Dta.samples.iterrows():
+            row[a.name] = ('%.3f' % ti) + ('-' if ti <= row[a.name] else '+')
+
+        at = Attribute(a.name, {('%.3f-' % ti), ('%.3f+' % ti)})
+        gain_D_a_ti = gain_discrete(Dta, at)
+        if gain_D_a_ti > gain:
+            gain = gain_D_a_ti
+            t = ti
+    return (gain, t)   
+
+
+def gain_discrete(D: TrainingSet, a: Attribute) -> float:
+    """计算样本 D 在离散值属性 a 上的信息增益(information entropy).
     """
     # print("Training set:\n%s\n" % D)
     ent_of_D = ent(D)
@@ -177,7 +225,15 @@ def gain(D: TrainingSet, a: Attribute) -> float:
     # print('Gain(%s) = %.3f' % (a.name, gain))
     return gain   
 
-def selet_best_attribute_method_ID3(D: TrainingSet, A: set) -> Attribute:
+def gain(D: TrainingSet, a: Attribute) -> float:
+    """计算样本 D 在属性 a 上的信息增益(information entropy).
+    """
+    if a.is_continuous:
+        return gain_continuous(D, a)
+    else:
+        return gain_discrete(D, a)
+
+def selet_best_attribute_method_ID3(D: TrainingSet, A: set) -> Tuple[Attribute, float]:
     """ID3决策树的分类选择算法.
     用信息增益来进行决策树的划分属性选择, 选择属性a* = arg max Gain(D,a).
 
@@ -196,14 +252,50 @@ def selet_best_attribute_method_ID3(D: TrainingSet, A: set) -> Attribute:
     max_attr = Attribute()
     max_attr_gain = 0
     for a in A:
-        gain_a = gain(D, a);
+        gain_a = gain_discrete(D, a);
         if gain_a > max_attr_gain:
             max_attr_gain = gain_a
             max_attr = a
     # print('Attr: %s, gain: %.3f' % (max_attr.name, max_attr_gain))
     # print(A)
     # print(D)
-    return max_attr
+    return (max_attr, 0)
+
+def selet_best_attribute_method_C4dot5(D: TrainingSet, A: set) -> Tuple[Attribute, float]:
+    """采用二分法(bi-partition)对连续属性进行处理，这正是C 4.5决策树算法.
+    用信息增益来进行决策树的划分属性选择, 选择属性a* = arg max Gain(D,a).
+
+    Parameters
+    ----------
+    D : TrainingSet
+        训练集 D = {(x1, y1), (x2, y2), ... , (xm, ym)};
+    A : set of Attribute
+        属性集 A = {a1, a2, ... , ad};
+
+    Returns
+    -------
+    best_attribute : Attribute
+        基于ID3选择的信息增益最大的属性.
+    """
+    max_attr = Attribute()
+    max_attr_gain = 0
+    max_attr_t = 0
+    for a in A:
+        gain_a = 0
+        t = 0
+        if a.is_continuous:
+            (gain_a, t) = gain_continuous(D, a)
+        else:
+            gain_a = gain_discrete(D, a)
+        if gain_a > max_attr_gain:
+            max_attr_gain = gain_a
+            max_attr = a
+            max_attr_t = t
+    # print('Attr: %s, gain: %.3f' % (max_attr.name, max_attr_gain))
+    # print(A)
+    # print(D)
+    return (max_attr, max_attr_t)
+
 
 def tree_generate(D: TrainingSet, A: set, selet_best_attribute_method, node_level_in_tree: int = 0) -> DecisionTreeNode : 
     """决策树学习基本算法的实现
@@ -249,7 +341,7 @@ def tree_generate(D: TrainingSet, A: set, selet_best_attribute_method, node_leve
         return node
     
     #8: 从A中选择最优划分属性a*(best_attribute)
-    classify_attribute = selet_best_attribute_method(D, A)
+    (classify_attribute, t) = selet_best_attribute_method(D, A)
     node.classify_name = classify_attribute.name
     # 9: for a* 的每一个值 av* do
     # 10:   为 node 生成一个分支；另Dv表示 D 中在 a* 上取值为 av* 的样本子集；
@@ -260,20 +352,23 @@ def tree_generate(D: TrainingSet, A: set, selet_best_attribute_method, node_leve
     # 15:   end if
     # 16:end for
 
-    for classify_value in classify_attribute.values:
-        
-        Dv = D.subset_by_attr(classify_attribute.name, classify_value)
+    if classify_attribute.is_continuous:
+        Dv_dict = D.bi_partition_by_attr(classify_attribute.name, t)
+    else:
+        Dv_dict = D.partition_by_attr(classify_attribute)
+    
+    for classify_value in Dv_dict:
+        Dv = Dv_dict[classify_value]
         if Dv.samples.empty:
             childNode = DecisionTreeNode(level = node_level_in_tree + 1)
             childNode.is_leaf = True
             childNode.label = majority_in_list(D.samples[D.label_name].to_list())
-            node.children[classify_value] = childNode
         else:
             sub_a = A.copy()
             sub_a.remove(classify_attribute)
             # print('%s=%s:' % (classify_attribute, classify_value))
             childNode = tree_generate(Dv, sub_a, selet_best_attribute_method, node_level_in_tree + 1)
-            node.children[classify_value] = childNode
+        node.children[classify_value] = childNode
             
     return node
 
@@ -287,11 +382,11 @@ if __name__ == '__main__':
             Attribute('敲声', {'沉闷', '浊响', '清脆'}),
             Attribute('纹理', {'清晰', '稍糊', '模糊'}),
             Attribute('脐部', {'凹陷', '稍凹', '平坦'}),
-            Attribute('触感', {'硬滑', '软粘'})
-            # Attribute('密度', {}),
-            # Attribute('含糖率', {}),
+            Attribute('触感', {'硬滑', '软粘'}),
+            Attribute('密度', True),
+            Attribute('含糖率', is_continuous = True)
         }
-    df = pd.read_csv('西瓜数据集 2.0.csv')
+    df = pd.read_csv('西瓜数据集 3.0.csv')
     df.set_index('编号', inplace=True)
     D = TrainingSet(df, '好瓜')
 
